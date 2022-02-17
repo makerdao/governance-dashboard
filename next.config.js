cfa32452e9b497e7fa0a74f9080904169d3016ff
Hernando Guzman <hernando.guzman93@gmail.com>
2022-02-17 15:44:12 -0300

    Add env var to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NETWORK_PROVIDER: process.env.NETWORK_PROVIDER || '',
+  },
 }
 
 module.exports = nextConfig
