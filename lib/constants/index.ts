51b3bddc94292efc15c6f7d0bcf1caca7bceb92e
Hernando Guzmán <A7XHernando@gmail.com>
2022-07-13 12:39:14 -0300

    Add delegates constant data

---
+++ lib/constants/index.ts
@@ -0,0 +1,105 @@
+export const DELEGATE_PLATFORMS = [
+  {
+    name: 'Flip Flop Flap Delegate LLC',
+    platform:
+      'https://forum.makerdao.com/t/flip-flop-flap-delegate-llc-platform/9106',
+    members: ['ElProgreso'],
+  },
+  {
+    name: 'Hasu',
+    platform: 'https://forum.makerdao.com/t/hasu-delegate-platform/13892',
+    members: ['Hasu'],
+  },
+  {
+    name: 'Feedblack Loops LLC',
+    platform:
+      'https://forum.makerdao.com/t/feedblack-loops-llc-delegate-platform-fbl/9366',
+    members: ['twblack88'],
+  },
+  {
+    name: 'Doo',
+    platform: 'https://forum.makerdao.com/t/doo-delegate-platform/13246',
+    members: ['Doo_Nam'],
+  },
+  {
+    name: 'JustinCase',
+    platform: 'https://forum.makerdao.com/t/justincase-delegate-platform/11721',
+    members: ['JustinCase'],
+  },
+  {
+    name: 'schuppi',
+    platform: 'https://forum.makerdao.com/t/schuppi-delegate-platform/11193',
+    members: ['ultraschuppi'],
+  },
+  {
+    name: 'Flipside Crypto',
+    platform:
+      'https://forum.makerdao.com/t/flipside-crypto-delegate-platform/13456',
+    members: ['rspa', 'fig', 'dav', 'dobby', 'avi'],
+  },
+  {
+    name: 'Penn Blockchain',
+    platform:
+      'https://forum.makerdao.com/t/penn-blockchain-delegate-platform/14419',
+    members: ['Juanbug'],
+  },
+  {
+    name: 'GFX Labs',
+    platform: 'https://forum.makerdao.com/t/gfx-labs-delegate-platform/11556',
+    members: ['GFXlabs'],
+  },
+  {
+    name: 'Chris Blec',
+    platform: 'https://forum.makerdao.com/t/chris-blec-delegate-platform/15544',
+    members: ['ChrisBlec'],
+  },
+  {
+    name: 'MakerMan',
+    platform: 'https://forum.makerdao.com/t/makerman-delegate-platform/9891',
+    members: ['MakerMan'],
+  },
+  {
+    name: 'ACREinvest',
+    platform: 'https://forum.makerdao.com/t/acreinvest-delegate-platform/11943',
+    members: ['ACREinvest'],
+  },
+  {
+    name: 'gauntlet',
+    platform: 'https://forum.makerdao.com/t/gauntlet-delegate-platform/9675',
+    members: ['Matt', 'inkymaze'],
+  },
+  {
+    name: 'mhonkasalo & teemulau',
+    platform:
+      'https://forum.makerdao.com/t/mhonkasalo-teemulau-delegate-platform/15473',
+    members: ['mhonkasalo', 'teemulau'],
+  },
+  {
+    name: 'Governance House',
+    platform:
+      'https://forum.makerdao.com/t/governance-house-delegate-platform/15191',
+    members: ['GovernanceHouse'],
+  },
+  {
+    name: 'Blockchain@Columbia',
+    platform:
+      'https://forum.makerdao.com/t/blockchain-columbia-delegate-platform/15202',
+    members: ['blockchainatcolumbia'],
+  },
+  {
+    name: 'monetsupply',
+    platform: 'https://forum.makerdao.com/t/monetsupply-delegate-platform/9545',
+    members: ['monet-supply'],
+  },
+  {
+    name: 'London Business School',
+    platform:
+      'https://forum.makerdao.com/t/london-business-school-delegate-platform/15203',
+    members: ['clmns', 'Alex_Kearney', 'cryptonativephd'],
+  },
+  {
+    name: 'ChicagoDAO',
+    platform: 'https://forum.makerdao.com/t/chicagodao-delegate-platform/15358',
+    members: ['sm_UC', 'mv_UC', 'af_UC'],
+  },
+]
